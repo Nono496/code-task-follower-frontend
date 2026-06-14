f02916c0ca3aff52b70bc2922e7752ad1d7e0c34
eleonore <82464261+Nono496@users.noreply.github.com>
2026-06-14 14:32:05 +0200

    PrimeNG + auth UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
+import { Dashboard } from './dashboard/dashboard';
+import { Signin } from './auth/signin/signin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'dashboard/:user-id',
+        component: Dashboard,
+        title: 'Dashboard'
+    },
+    {
+        path: 'login',
+        component: Login,
+        title: 'Log in'
+    },
+    {
+        path: 'signin',
+        component: Signin,
+        title: 'Sign in'
+    },
+    {
+        path: '**',
+        redirectTo: '/login'
+    }
+];
---
+++ src/app/auth/login/login.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { RouterLink } from "@angular/router";
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+
+import { inject } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MessageModule } from 'primeng/message';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { MessageService } from 'primeng/api';
+import { AuthService } from '../auth-service';
+
+@Component({
+  selector: 'app-login',
+  imports: [RouterLink, FloatLabelModule, InputTextModule, FormsModule, MessageModule, ToastModule, ButtonModule, ReactiveFormsModule],
+  providers: [AuthService, MessageService],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  authService = inject(AuthService);
+
+  messageService = inject(MessageService);
+  fb = inject(FormBuilder);
+
+  form: FormGroup;
+  formSubmitted: boolean = false;
+
+  constructor() {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    // TODO Call AuthService
+      this.formSubmitted = true;
+      if (this.form.valid) {
+          this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Form Submitted', life: 3000 });
+          this.form.reset();
+          this.formSubmitted = false;
+      }
+  }
+
+  getControl(controlName: string) {
+    return this.form.get(controlName) as FormControl;
+  }
+  isInvalid(controlName: string) {
+      const control = this.getControl(controlName);
+    
+      return control?.invalid && (control.dirty || this.formSubmitted);
+  }
+}
